12b6e850a0179b65c7ce188fa0b15c8b395ac610
Castro <onericky16@gmail.com>
2020-05-27 00:16:12 -0500

    variables z x w on sidenav

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,11 @@ class DashboardController < ApplicationController
   def index
     mTT = TableTest.new
 
-    @value = mTT.getTest()
+    # @value = mTT.getTest()
     @yes = "Si"
+  end
+
+  def ies
+
   end
 end
